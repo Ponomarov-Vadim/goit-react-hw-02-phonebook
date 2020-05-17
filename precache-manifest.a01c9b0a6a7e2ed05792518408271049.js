78de0ad93cde4bf2d230fbe773ef3b915cd9036b
Vadim Ponomarov <remary300@gmail.com>
2020-05-17 14:36:52 +0300

    Updates

---
+++ precache-manifest.a01c9b0a6a7e2ed05792518408271049.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd448c0f4f25b504d08e1d1ac7d8d597",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "86261e81bbcc012fa526",
+    "url": "/goit-react-hw-01-components/static/css/main.b623dcdc.chunk.css"
+  },
+  {
+    "revision": "2e0d19fc245920e724cd",
+    "url": "/goit-react-hw-01-components/static/js/2.423ab698.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/goit-react-hw-01-components/static/js/2.423ab698.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86261e81bbcc012fa526",
+    "url": "/goit-react-hw-01-components/static/js/main.0d301e5f.chunk.js"
+  },
+  {
+    "revision": "b373b8ec70543313076d",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.1115fa28.js"
+  }
+]);
